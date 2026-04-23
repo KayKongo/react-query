3208cd53ec657e6e8e6b2b30b609c50978366a25
Prince Kongo <kongoprince100@gmail.com>
2026-04-23 17:06:20 +0000

    learn: currently learning tanstack query

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,8 +1,29 @@
-import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+import { useQuery } from "@tanstack/react-query";
+import SingleItem from "./SingleItem";
+import customFetch from "./utils";
+
+// Main codebase
+const Items = () => {
+  const { isLoading, data, error } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: async () => {
+      const { data } = await customFetch.get("/");
+      return data;
+    },
+  });
+
+  // What to do when loading
+  if (isLoading) {
+    return <p style={{ marginTop: "1rem" }}>Loading...</p>;
+  }
+
+  if (error) {
+    return `Uh-oh, we got a ${error}!`;
+  }
+
   return (
-    <div className='items'>
-      {items.map((item) => {
+    <div className="items">
+      {data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
